6acc18278d8827823397083178a81fb822824b7e
sunnyallana <study.sunnyallana@gmail.com>
2024-03-15 13:36:05 +0500

    Added UI for all pages

--- ConvoNest/base/forms.py
+++ ConvoNest/base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     # Meta is used to specify the model and the fields that we want to use in the form. In this case, we are using the Room model and the fields name, topic, and description.
@@ -7,3 +8,10 @@ class RoomForm(ModelForm):
         model = Room
         fields = '__all__' # This is how you include all the fields from the model in the form. In this case, we are including all the fields from the Room model in the form.
         exclude = ['participants', 'host'] # This is how you exclude fields from the form. In this case, we are excluding the participants and host fields from the form. We don't want the user to be able to set these fields when creating a new room. We want to set these fields in the view function instead.
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        exclude = ['password', 'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined']
--- ConvoNest/base/views.py
+++ ConvoNest/base/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Messages
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.db.models import Q
 from django.http import HttpResponse
 
@@ -121,31 +121,57 @@ def userProfile(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        
         # Instead of using the Room model directly, we use the RoomForm that we created in the forms.py file. This form is a ModelForm that is created from the Room model. We pass the request.POST to the form to create a new Room object.
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False) # Buying ourselves some time to modify the room object before saving it to the database.
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit=False) # Buying ourselves some time to modify the room object before saving it to the database.
+        #     room.host = request.user
+        #     room.save()
+        return redirect('home')
+    context = {'form': form,
+               'topics': topics,
+               }
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse("You are not allowed here")
     # RoomForm() gives you an empty form, and RoomForm(instance=room/initial=room) gives you a form that is pre-filled with the data from the room object.
     form = RoomForm(instance=room)
     if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name=request.POST.get('name')
+        room.topic=topic
+        room.description=request.POST.get('description')
+        room.save()
+
         # This updates the room object with the new data from the form and saves it to the database. We then redirect the user to the home page.
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        # form = RoomForm(request.POST, instance=room)
+        # if form.is_valid():
+        #     form.save()
+       
+       
+        return redirect('home')
+    context = {'form': form,
+               'topics': topics,
+               'room': room,
+               }
     return render(request, 'base/room_form.html', context)
 
 
@@ -173,4 +199,19 @@ def deleteMessage(request, pk):
     return render(request, 'base/delete.html', {'object': message})
 
 
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
 
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'base/update-user.html', context)
